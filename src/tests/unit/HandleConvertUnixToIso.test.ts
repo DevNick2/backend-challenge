79f4b570d49bcc03a7e7ce43107bdc8d1912fc3c
Jean Nicolas <jeannicolasav@gmail.com>
2024-06-23 10:40:22 -0300

    Feat: Suite de testes para o handleConvertUnixToIso
    Adicionado a suite de testes para testar a funcao handleConvertUnixToIso, deve receber um unix e devolver um ISO 8601 com o UTC

--- src/tests/unit/HandleConvertUnixToIso.test.ts
+++ src/tests/unit/HandleConvertUnixToIso.test.ts
@@ -1,14 +1,19 @@
+import { handleConvertUnixToIso } from '../../controllers/UserController';
+import { UserInterface } from '../../resources/UserInterface';
+
+import { newUser } from '../UserFactory';
+
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 
 dayjs.extend(utc);
 
 describe('Valida se a funcionalidade de conversão de data retorna no ISO 8601', () => {
-  // importar a função
-  // criar um user com um last_activity unix
-  // cenario: a data unix deve retornar ISO 8601
-  // importar o método para criar usuarios
+  let user: UserInterface
 
-  const user = {}
-  expect(handleConvertUnixToIso(user.last_activity)).toBe(dayjs(user.last_activity).utc(true).toISOString())
+  beforeEach(() => user = newUser())
+
+  test('A data em unix deve retornar um ISO 8601 com o UTC correto', () => {
+    expect(handleConvertUnixToIso(user.last_activity)).toBe(dayjs(user.last_activity).utc(true).toISOString())
+  })
 })
